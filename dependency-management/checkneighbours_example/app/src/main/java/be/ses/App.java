cf6916e5f3a50d80007e35ac2a449db43ab6a92a
RubenDemeer <ruben.demeer@gmail.com>
2025-03-14 10:28:14 +0100

    opdracht 6 debugged

--- dependency-management/checkneighbours_example/app/src/main/java/be/ses/App.java
+++ dependency-management/checkneighbours_example/app/src/main/java/be/ses/App.java
@@ -14,7 +14,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        ArrayList<Integer> grid = new ArrayList<>(Arrays.asList(0, 0, 1, 0, 1, 1, 0, 2, 2, 0, 1, 3, 0, 1, 1, 1));
+        int[] grid = {0, 0, 1, 0, 1, 1, 0, 2, 2, 0, 1, 3, 0, 1, 1, 1};
         System.out.println(CheckNeighboursInGrid.getSameNeighboursIds(grid, 4, 4, 5) + " This should be [2,4,10]");
     }
 }
