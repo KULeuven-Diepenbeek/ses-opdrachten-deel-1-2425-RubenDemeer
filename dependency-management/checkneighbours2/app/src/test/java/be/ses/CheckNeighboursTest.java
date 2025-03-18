ea7f7925247a6188a94ba1c3df6d221fc531376a
RubenDemeer <ruben.demeer@gmail.com>
2025-03-18 17:18:45 +0100

    eerste test werkt

--- dependency-management/checkneighbours2/app/src/test/java/be/ses/CheckNeighboursTest.java
+++ dependency-management/checkneighbours2/app/src/test/java/be/ses/CheckNeighboursTest.java
@@ -1,13 +1,17 @@
 package be.ses;
 
 import org.junit.Test;  
-import static org.junit.Assert.*;
+// import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+import java.util.ArrayList;
 
 public class CheckNeighboursTest {
 
   
   @Test
-  public void gegevenTeller2Noemer4_wanneerDivide_danResult0point5() {
+  public void gegevenIndexIs5_resultIs() {
     // 1. Arrange
     CheckNeighboursInGrid checkNeighboursInGrid = new CheckNeighboursInGrid();
     int [] grid = new int[]{  0, 0, 1, 0,
@@ -16,12 +20,16 @@ public class CheckNeighboursTest {
                               0, 1, 1, 1 };
 
     // 2. Act
-    Iterable<Integer> result = checkNeighboursInGrid.getSameNeighboursIds(grid, 4, 4, 5);
+    ArrayList<Integer> result = (ArrayList<Integer>) checkNeighboursInGrid.getSameNeighboursIds(grid, 4, 4, 5);
+    
     System.out.println(result);
 
     // 3. Assert
-    assertThat(result).isEqualTo(new int[]{ 2, 4, 10});
-
+    int [] oplossing = new int[]{2, 4, 10};
     
+    for (int i = 0; i < 3; i++){
+      assertThat(result.get(i)).isEqualTo(oplossing[i]);
+    }
+       
   }
 }
